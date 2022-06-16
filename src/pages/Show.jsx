d985db112455c34257d4d927d66f222076eb99ea
dhannjayyy <d.dhannjayyy@gmail.com>
2022-06-17 00:35:20 +0530

    replaced useState(S) with the useReducer

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,35 +1,58 @@
-import React, { useEffect,useState } from 'react'
-import { useParams } from 'react-router'
-import GET_API from '../components/misc/getapi';
+import React, { useEffect, useReducer } from "react";
+import { useParams } from "react-router";
+import GET_API from "../components/misc/getapi";
+
+const initialState = {
+    show: null,
+    isLoading: true,
+    error: null,
+};
+
+const reducer = (prevState, action) => {
+    switch (action.type) {
+        case "FETCH_SUCCESS": {
+            return { isLoading: false, error: null, show: action.show };
+        }
+        case "FETCH_FAILED": {
+            return { ...prevState, isLoading: false, error: action.error };
+        }
+
+        default:
+            return prevState;
+    }
+};
 
 const Show = () => {
     const { id } = useParams();
-    const [show,setShow] = useState(null);
-    const [isLoading,setIsLoading] = useState(true);
-    const [error,setError] = useState(null);
+
+    const [{show,isLoading,error}, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
         let isMounted = true;
-        GET_API(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-            if(isMounted){
-                setIsLoading(false);
-                setShow(results);
-            }
-        }).catch(err=>{
-            if(isMounted){
-                setIsLoading(false);
-                setError(err.message);
-            }
-        })
-        
-        return ()=>{
-            isMounted=false;
-        }
-    }, [id])
-    
-    return (
-        <div>This is show page</div>
-    )
-}
+        GET_API(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+            .then((results) => {
+                if (isMounted) {
+                    dispatch({
+                        type: "FETCH_SUCCESS",
+                        show: results,
+                    });
+                }
+            })
+            .catch((err) => {
+                if (isMounted) {
+                    dispatch({
+                        type: "FETCH_FAILED",
+                        error: err.message,
+                    });
+                }
+            });
 
-export default Show
+        return () => {
+            isMounted = false;
+        };
+    }, [id]);
+    console.log(show,isLoading,error)
+    return <div>This is show page</div>;
+};
+
+export default Show;
