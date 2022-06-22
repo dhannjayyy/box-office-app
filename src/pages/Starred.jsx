d3b46a47d2f8c8bc79ce081ac71f4f83fe7c2f1c
dhannjayyy <d.dhannjayyy@gmail.com>
2022-06-23 01:18:50 +0530

    completed starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useEffect, useState } from "react";
+import MainPageLayout from "../components/MainPageLayout";
+import { useShows } from "../components/misc/custom-hooks";
+import ShowGrid from "../components/show/ShowGrid"
+import GET_API from "../components/misc/getapi";
 
 const Starred = () => {
-  return (
-    <MainPageLayout>Starred</MainPageLayout>
-  )
-}
+  const [starred] = useShows();
 
-export default Starred
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const Promises = starred.map((showId) => GET_API(`/shows/${showId}`));
+      console.log(Promises);
+
+      Promise.all(Promises)
+        .then(apiData => apiData.map((show) => ({ show })))
+        .then(results => {
+          console.log('results',results);
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return <MainPageLayout>
+    {isLoading && <div> Shows are loading </div>}
+    {error && <div> Something went wrong {` ${error}`} </div>}
+    {!isLoading && !shows && <div> No shows were added </div>}
+    {!isLoading && !error && shows && <div> <ShowGrid data={shows}/> </div>}
+  </MainPageLayout>;
+};
+
+export default Starred;
