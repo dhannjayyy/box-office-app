f8743124961e9dd7229a7a3dd2de3dc7cc194347
dhannjayyy <d.dhannjayyy@gmail.com>
2022-06-27 22:02:32 +0530

    defined theme for styled components and styled some components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import { Switch, Route } from "react-router";
 import Home from "./pages/Home";
 import Show from "./pages/Show";
 import Starred from "./pages/Starred";
+import { ThemeProvider } from "styled-components";
+
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-<Route exact path="/show/:id">
-<Show/>
-</Route>
-
-      <Route>
-        <div>This is 404 page</div>
-      </Route>
-    </Switch>
+        <Route>
+          <div>This is 404 page</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
 
   );
 }
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { TitleWrapper } from './Title.styled';
 
 const Title = (props) => {
   return (
-    <>
+    <TitleWrapper>
     <h1>{props.title}</h1>
     <p>{props.subTitle}</p>
-    </>
+    </TitleWrapper>
   )
 }
 
